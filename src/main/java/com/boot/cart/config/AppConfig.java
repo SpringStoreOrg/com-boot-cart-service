ef6afeae416dbd9b42e99b4c46e1db69146dc2e1
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2021-12-17 12:15:08 +0200

    cors filters removed

--- src/main/java/com/boot/cart/config/AppConfig.java
+++ src/main/java/com/boot/cart/config/AppConfig.java
@@ -3,9 +3,6 @@ package com.boot.cart.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import com.boot.cart.client.ProductServiceClient;
 import com.boot.cart.client.UserServiceClient;
@@ -13,21 +10,6 @@ import com.boot.cart.client.UserServiceClient;
 @Configuration
 public class AppConfig {
 	
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-}
 	@Bean
 	public RestTemplate template() {
 	    return new RestTemplate();
