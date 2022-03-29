b32fabbda9dc175b13c308f6afccdfcd107f4701
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-03-29 17:29:20 +0300

    heroku configs added

--- src/main/java/com/boot/cart/util/Constants.java
+++ src/main/java/com/boot/cart/util/Constants.java
@@ -2,11 +2,13 @@ package com.boot.cart.util;
 
 public class Constants {
 
-	public static final String BASE_URL_USER = "http://localhost:8080/";
+	//public static final String BASE_URL_USER = "http://localhost:8080/";
+	public static final String BASE_URL_USER = "https://spring-store-user-service.herokuapp.com/";
 	public static final String GET_USER_BY_EMAIL = BASE_URL_USER + "getUserByEmail?email=";
 	public static final String UPDATE_USER = BASE_URL_USER + "updateUserByUserName/";
 	
-	public static final String BASE_URL_PRODUCT = "http://localhost:8081/";
+	//public static final String BASE_URL_PRODUCT = "http://localhost:8081/";
+	public static final String BASE_URL_PRODUCT = "https://spring-store-product-service.herokuapp.com/";
 	public static final String GET_PRODUCT_BY_PRODUCT_NAME = BASE_URL_PRODUCT + "getProductByProductName?productName=";
 	public static final String UPDATE_PRODUCT_BY_PRODUCT_NAME = BASE_URL_PRODUCT + "updateProductByProductName/";
 	public static final String GET_ALL_PRODUCTS = BASE_URL_PRODUCT + "getAllProducts";
