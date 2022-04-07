ad5d66f6d5bbc367226024c9324096a8e968752d
florinv23 <100446582+florinv23@users.noreply.github.com>
2022-04-07 22:05:17 +0300

    Possible improvement in service url usage

--- src/main/java/com/boot/cart/util/Constants.java
+++ src/main/java/com/boot/cart/util/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
 	//public static final String BASE_URL_USER = "http://localhost:8080/";
 	public static final String BASE_URL_USER = "https://spring-store-user-service.herokuapp.com/";
+	//TODO instead of hardcoding this url in here you could put them in application.properties file and inject them in the Config class where you create the RestTemplate using @Value("${user.service.url}"). Value annotation is from org.springframework.beans.factory.annotation. 
 	public static final String GET_USER_BY_EMAIL = BASE_URL_USER + "getUserByEmail?email=";
 	public static final String UPDATE_USER = BASE_URL_USER + "updateUserByUserName/";
 	
