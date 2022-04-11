824591c955b8258d941c0faee8980c84940aab40
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-04-11 18:07:30 +0300

    separate rest templates created

--- src/main/java/com/boot/cart/config/AppConfig.java
+++ src/main/java/com/boot/cart/config/AppConfig.java
@@ -19,7 +19,6 @@ public class AppConfig {
     @Value("${product.service.url}")
     private String productServiceUrl;
 
-    //TODO here I would create 2 restTemplates using something like new RestTemplateBuilder().rootUri("http://user.service.url").build() and use just relative paths and reference them in the client classes by name.
     @Bean(name = "userServiceRestTemplate")
     public RestTemplate userServiceRestTemplateUrl() {
         return new RestTemplateBuilder().rootUri(userServiceUrl).build();
