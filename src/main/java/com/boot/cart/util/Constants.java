52929d084455fe9b7ffb8a9e313c4361e9f18c30
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-05-18 14:20:27 +0300

    Update Constants.java

--- src/main/java/com/boot/cart/util/Constants.java
+++ src/main/java/com/boot/cart/util/Constants.java
@@ -4,9 +4,9 @@ public class Constants {
 
     public static final String GET_USER_BY_EMAIL = "/?email={email}";
 
-    public static final String GET_PRODUCT_BY_PRODUCT_NAME = "/getProductByProductName?productName={productName}";
-    public static final String UPDATE_PRODUCT_BY_PRODUCT_NAME = "/updateProductByProductName/{productName}";
-    public static final String GET_ALL_PRODUCTS = "/getAllProducts";
+    public static final String GET_PRODUCT_BY_PRODUCT_NAME = "/?productName={productName}";
+    public static final String UPDATE_PRODUCT_BY_PRODUCT_NAME = "/{productName}";
+    public static final String GET_ALL_PRODUCTS = "/products";
 
     //Regular expression used for email validation
     public static final String EMAIL_REGEXP = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
