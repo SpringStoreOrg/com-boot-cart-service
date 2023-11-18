c91bd01de90f26a7577f460f56d067ef21be27c2
florinv23 <100446582+florinv23@users.noreply.github.com>
2023-11-19 00:10:24 +0200

    Clean properties

--- src/main/java/com/boot/cart/SpringCartServiceApplication.java
+++ src/main/java/com/boot/cart/SpringCartServiceApplication.java
@@ -2,17 +2,13 @@ package com.boot.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableScheduling
 @EnableFeignClients
-@ConditionalOnProperty(name = "spring.enable.scheduling")
 @EntityScan(basePackages = {"com.boot.cart.model"})
 public class SpringCartServiceApplication {
 
